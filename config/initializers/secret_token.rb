e5dd66dd27bb40328dc3f89d03d382e7260077bf
chris hoffman <hoffmanc@dostoevsky.hsd1.nh.comcast.net>
2011-02-22 07:52:22 -0500

    ttwrj site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ttwrj::Application.config.secret_token = '3f1aa8bb06a294a3836279dcd3f8b0424f1fcc0bac916655756bcd4b0bfdc9c6959dabebd0ae43160f082f95ead0aed4695be1a0e4d441859479073904a4cb85'
